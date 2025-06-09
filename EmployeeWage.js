22dd60cfe73748e8560006866c18faba76d9b993
Rupesh R Sahoo <rs0722@srmist.edu.in>
2025-06-09 14:21:52 +0530

    Uc3 : adding wage Function

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,9 +1,18 @@
+//uc1 : Attendance check
 function checkAttendance() {
     const isPresent = Math.random() < 0.5;
     console.log(isPresent ? "Employee is Present" : "Employee is Absent");
     return isPresent;
 }
 
+// //uc2 : wage calculation
+// function calculateWage(empCheck) {
+//     const wagePerHour = 20;
+//     const empHrs = empCheck === 1 ? 4 : empCheck === 2 ? 8 : 0;
+//     return empHrs * wagePerHour;
+// }
+
+//uc3: wage function
 function getWorkingHours(empCheck) {
     switch (empCheck) {
         case 1: return 4;
@@ -17,12 +26,12 @@ function calculateWage(empCheck) {
     return getWorkingHours(empCheck) * wagePerHour;
 }
 
-// Connected:
+
+// first check attendance and calculate the wage
 if (checkAttendance()) {
     const empCheck = Math.floor(Math.random() * 3);
     console.log(`Employee Daily Wage: $${calculateWage(empCheck)}`);
 } else {
     console.log("No wage — Employee absent.");
 }
-
 // calculateWage();
